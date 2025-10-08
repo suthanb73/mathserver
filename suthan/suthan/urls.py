5a6f3a10ce18d28fe3ca9b4ac49e8d669bb65e30
suthanb73 <suthanb73@gmail.com>
2025-10-08 15:35:04 +0530

    Success

---
+++ suthan/suthan/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path
+from mathapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.calculate_power,name="calculate_power"),
+]
